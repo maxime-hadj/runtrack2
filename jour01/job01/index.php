5be0ef1b58d67bee042fa05153a9a7cb3fc5c6df
Max <maxime.hadj@laplateforme.io>
2021-11-02 17:10:28 +0100

    ligne php 2

---
+++ jour01/job01/index.php
@@ -0,0 +1,25 @@
+<?php
+
+$str2 = "Vive ";
+echo "$str2";
+
+$str = "LaPlateforme";
+echo "$str";
+
+$str3 = "!";
+echo "$str3", "<br>";
+
+$val = "6";
+echo "$val ";
+echo "$val" + "4", "<br>";
+
+
+$myBool = "true";
+echo "myBool";
+
+$myBool = "false";
+echo "myBool";
+
+
+
+?>
