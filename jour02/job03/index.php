7a56ce628bcea690b57fb4f1679918f9f5696441
Max <maxime.hadj@laplateforme.io>
2021-11-04 09:57:02 +0100

    jour02 job01 02 03
    3/11

---
+++ jour02/job03/index.php
@@ -0,0 +1,24 @@
+<?php
+
+for ($i=0; $i<=100; $i++) { 
+	if ($i>=0 && $i<=20) {
+		echo "<i>" . "$i" . "</i>" . "<br/>";
+	}
+	
+	elseif ($i==42) {
+		echo "LaPlateforme_" . "<br/>";
+	}
+
+	elseif ($i>=25 && $i<=50) {
+		echo "<u>" . "$i" . "</u>" . "<br/>";
+	}
+
+	else {
+		echo "$i" . "<br/>";
+
+	}
+
+
+
+}
+?>
