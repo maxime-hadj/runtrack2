aa8d163a0bd76a629343e67a51ac605256ff6b97
Max <maxime.hadj@laplateforme.io>
2021-11-05 10:59:25 +0100

    jour04
    job01

---
+++ jour04/job01/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Doc</title>
+</head>
+
+<body>
+
+<form action="index.php" method="get">
+	<label>Name :</label>
+	<input type="text" name="name">
+	<input type="submit" value="submit">
+	<label>Surname :</label>
+	<input type="text" name="surname">
+	<input type="submit" value="submit">
+</form>
+
+</body>
+</html>
+
+<?php
+
+if (isset($_GET['name'])) {
+	echo "Hello" . $_GET['name'] . "!";
+
+if (isset($_GET['surname'])) {
+	echo "Hello" . $_GET['surname'] . "!";
+	}	
+}
+
+var_dump($_GET['name'] ['surname']);
+
+?>
