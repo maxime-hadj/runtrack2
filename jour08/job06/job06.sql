881475c6e7d7ce61db5ec650723f97b5d9bd0379
Max <maxime.hadj@laplateforme.io>
2021-11-10 14:35:51 +0100

    modif

--- jour08/job06/job06.sql
+++ jour08/job06/job06.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 10, 2021 at 02:26 PM
+-- Generation Time: Nov 10, 2021 at 02:34 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
